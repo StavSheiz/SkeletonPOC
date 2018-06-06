808df4a291e9eb7aa4502fedcae631f977e6bffd
HuMMeRSI <sagih95@gmail.com>
2018-06-06 10:51:31 +0300

    Lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,12 @@ module.exports = {
 	'linebreak-style': 'off',
 	'comma-dangle': 'off',
 	'semi': ['error', 'always'],
-	'import/no-unresolved': 'off'
+	'import/no-unresolved': 'off',
+	'import/no-extraneous-dependencies': 'off',
+	'import/no-duplicates': 'off',
+	'import/extensions': 'off',
+	'import/no-named-as-default': 'off',
+	'import/no-named-as-default-member': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
